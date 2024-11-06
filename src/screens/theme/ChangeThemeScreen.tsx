c775cb7053c2505aaa19d51edae0b2153feb80f6
Juan Camilo <juancamilomendozavillegas14@gmail.com>
2024-11-05 22:34:59 -0500

    feat: add infinite scroll with custom hook for animations and loaders
    - Implemented infinite scroll for images, loading more content as the user scrolls down
    - Created a custom hook for animations to enhance user experience during content loading
    - Added loading indicators to inform users that more content is being fetched
    - Optimized loading and performance to ensure smooth scrolling and minimal interruptions

--- src/screens/theme/ChangeThemeScreen.tsx
+++ src/screens/theme/ChangeThemeScreen.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { TitleComponent } from '../../components/ui/titleComponent'
+import { CustomView } from '../../components/ui/CustomView'
 
 export const ChangeThemeScreen = () => {
   return (
-    <View>
-      <Text>ChangeThemeScreen</Text>
-    </View>
+    <CustomView>
+      <TitleComponent title='Theme Changer'/>
+    </CustomView>
   )
 }
